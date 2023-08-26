1b31bf791df2de99b1c0ed9e1101635c9ec1f576
Bibelot <jakurem@protonmail.com>
2023-08-26 13:28:18 +0200

    Chapter 9 Exercise 1

--- src/main.cpp
+++ src/main.cpp
@@ -177,7 +177,7 @@ int main() {
   view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
   glm::mat4 projection;
-  projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+  projection = glm::perspective(glm::radians(30.0f), 100.0f / 200.0f, 0.1f, 100.0f);
 
   shader.setMat4("view", view);
   shader.setMat4("projection", projection);
