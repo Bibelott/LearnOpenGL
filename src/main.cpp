b34192d0b49c7b2833e982e2f24b5e39f4eebb8c
Bibelot <jakurem@protonmail.com>
2023-08-27 13:30:50 +0200

    Round, Round, Round, Round

--- src/main.cpp
+++ src/main.cpp
@@ -122,7 +122,7 @@ int main() {
 
   // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-  // stbi_set_flip_vertically_on_load(true);
+  stbi_set_flip_vertically_on_load(true);
 
   unsigned int texture[2];
   glGenTextures(2, texture);
@@ -173,13 +173,9 @@ int main() {
   shader.setInt("texture1", 0);
   shader.setInt("texture2", 1);
 
-  glm::mat4 view = glm::mat4(1.0f);
-  view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
   glm::mat4 projection;
   projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
-  shader.setMat4("view", view);
   shader.setMat4("projection", projection);
 
   glEnable(GL_DEPTH_TEST);
@@ -194,12 +190,18 @@ int main() {
       glm::mat4 model = glm::mat4(1.0f);
       model = glm::translate(model, cubePositions[i]);
       float angle = 20.0f * i;
-      if (i % 3 == 0) {
-        angle += (float)glfwGetTime() * 50.0f;
-      }
       model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-
       shader.setMat4("model", model);
+
+      const float radius = 10.0f;
+      float camX = sin(glfwGetTime()) * radius;
+      float camZ = cos(glfwGetTime()) * radius;
+
+      glm::mat4 view;
+      view = glm::lookAt(glm::vec3(camX, 0.0f, camZ),
+                         glm::vec3(0.0f, 0.0f, 0.0f),
+                         glm::vec3(0.0f, 1.0f, 0.0f));
+      shader.setMat4("view", view);
 
       glDrawArrays(GL_TRIANGLES, 0, 36);
     }
