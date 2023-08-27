694464e43d1c562ffadd0f7d6ca7bf13e01aa589
Bibelot <jakurem@protonmail.com>
2023-08-27 16:34:44 +0200

    Chapter 10 Exercise 1

--- include/camera.hpp
+++ include/camera.hpp
@@ -71,9 +71,9 @@ public:
     {
         float velocity = MovementSpeed * deltaTime;
         if (direction == FORWARD)
-            Position += Front * velocity;
+            Position += glm::vec3(Front.x, 0.0f, Front.z) * velocity;
         if (direction == BACKWARD)
-            Position -= Front * velocity;
+            Position -= glm::vec3(Front.x, 0.0f, Front.z) * velocity;
         if (direction == LEFT)
             Position -= Right * velocity;
         if (direction == RIGHT)
