b9826235ffabf5c117cbfe91f554ec3aeb5e48f4
Bibelot <jakurem@protonmail.com>
2023-08-30 14:34:07 +0200

    Diffuse Map

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+unsigned int loadTexture(const char *path);
 
 int main() {
   glfwInit();
@@ -127,6 +128,9 @@ int main() {
   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
 
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
   // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   glGenVertexArrays(1, &lightVAO);
@@ -139,14 +143,20 @@ int main() {
 
   glBindVertexArray(0);
 
+  unsigned int diffuseMap = loadTexture("textures/container2.png");
+  
   cubeShader.use();
-  cubeShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
-  cubeShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+  cubeShader.setInt("material.diffuse", 0);
   cubeShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
   cubeShader.setFloat("material.shininess", 32.0f);
 
   cubeShader.setVec3("light.position", lightPos);
+  cubeShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
+  cubeShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
   cubeShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, diffuseMap);
 
   glEnable(GL_DEPTH_TEST);
   // render loop
@@ -172,17 +182,6 @@ int main() {
       cubeShader.setMat4("projection", projection);
 
       cubeShader.setVec3("viewPos", camera.Position);
-
-      glm::vec3 lightColor;
-      lightColor.x = sin(glfwGetTime() * 2.0f);
-      lightColor.y = sin(glfwGetTime() * 0.7f);
-      lightColor.z = sin(glfwGetTime() * 1.3f);
-
-      glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f);
-      glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);
-
-      cubeShader.setVec3("light.ambient", ambientColor);
-      cubeShader.setVec3("light.diffuse", diffuseColor);
 
       glBindVertexArray(cubeVAO);
       glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -258,4 +257,38 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
   camera.ProcessMouseScroll(yoffset);
+}
+
+unsigned int loadTexture(char const * path) {
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if (data) {
+        GLenum format = 0;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    }
+    else {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+
+    return textureID;
 }
