650b478839b3dbdc833c36f582dbf3e4455e07ce
Bibelot <jakurem@protonmail.com>
2023-08-31 14:38:35 +0200

    Directional Lighting

--- src/main.cpp
+++ src/main.cpp
@@ -106,7 +106,19 @@ int main() {
      0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f, 1.0f, 0.0f,
     -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f, 0.0f, 0.0f,
     -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f, 0.0f, 1.0f
-};
+  };
+  glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,   0.0f),
+    glm::vec3( 2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f,  -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f,  -3.5f),
+    glm::vec3(-1.7f,  3.0f,  -7.5f),
+    glm::vec3( 1.3f, -2.0f,  -2.5f),
+    glm::vec3( 1.5f,  2.0f,  -2.5f),
+    glm::vec3( 1.5f,  0.2f,  -1.5f),
+    glm::vec3(-1.3f,  1.0f,  -1.5f)
+  };
 
   unsigned int cubeVAO, lightVAO, VBO;
   // unsigned int EBO;
@@ -151,7 +163,7 @@ int main() {
   cubeShader.setInt("material.specular", 1);
   cubeShader.setFloat("material.shininess", 32.0f);
 
-  cubeShader.setVec3("light.position", lightPos);
+  cubeShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
   cubeShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
   cubeShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
   cubeShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
@@ -174,9 +186,6 @@ int main() {
     {
       cubeShader.use();
 
-      glm::mat4 model = glm::mat4(1.0f);
-      cubeShader.setMat4("model", model);
-
       glm::mat4 view = camera.GetViewMatrix();
       cubeShader.setMat4("view", view);
 
@@ -185,9 +194,18 @@ int main() {
       cubeShader.setMat4("projection", projection);
 
       cubeShader.setVec3("viewPos", camera.Position);
-
       glBindVertexArray(cubeVAO);
-      glDrawArrays(GL_TRIANGLES, 0, 36);
+
+      for (unsigned int i = 0; i < 10; i++) {
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, cubePositions[i]);
+        float angle = 20.0f * i;
+        model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+        cubeShader.setMat4("model", model);
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+      }
+
       glBindVertexArray(0);
     }
     {
